1926f8049e619854a08746bd67e6b53f70a640ce
Sang Woo, Cho <55518121+nr97819@users.noreply.github.com>
2022-05-22 02:49:20 +0900

    Update and rename 220522_CookieRun_02.txt to 220522_CookieRun_02.cpp

--- 220522_CookieRun_02.cpp
+++ 220522_CookieRun_02.cpp
@@ -35,8 +35,24 @@ private:
 public:
     void Init()
     {
-       player. 
-    }
+        /* ------------------------ */
+        /*      핵심 추가 코드      */
+        /* ------------------------ */
+        CAnimation* anim_run = new CAnimation(0.);
+        CAnimation* anim_jump_start = new CAnimation(0.);
+        CAnimation* anim_jump2_start = new CAnimation(0.);
+        CAnimation* anim_jump2_spinFall = new CAnimation(0.);
+        CAnimation* anim_jump_fall = new CAnimation(0.);
+        CAnimation* anim_jump_end = new CAnimation(0.);
+        CAnimation* anim_slide = new CAnimation(0.);
+        
+        SetAnimation(ANIM_STATE::RUN, anim_run);
+        SetAnimation(ANIM_STATE::RUN, anim_jump_start);
+        SetAnimation(ANIM_STATE::RUN, anim_jump2_start);
+        SetAnimation(ANIM_STATE::RUN, anim_jump2_spinFall);
+        SetAnimation(ANIM_STATE::RUN, anim_jump_fall);
+        SetAnimation(ANIM_STATE::RUN, anim_jump_end);
+        SetAnimation(ANIM_STATE::RUN, anim_slide);
     
     void Update(){}
     void Render(){}
@@ -46,19 +62,25 @@ public:
 class CPlayer
 {
 private:
-    std::vector<CAnimation*> pArrAnims;
+    /* ------------------------ */
+    /*      핵심 추가 코드      */
+    /* ------------------------ */
+    
+    // CAnimation은 array로 구현하도록 수정하고,
+    // CSprite만 vector로 구현한 점
+    CAnimation* pArrAnims[ANIM_STATE::LAST_INDEX];
     Vec2 vPos;
     
 public:
     CPlayer(){}
     ~CPlayer()
     {
-        for(std::vector<CAnimation*>::iterator it = pArrAnims.Begin(); it != pArrAnims.End(); ++it)
+        for(UINT i = 0; i < ANIM_STATE::LAST_INDEX; ++i)
         {
-            if(nullptr != it.ptr)
+            if(nullptr != pArrAnims[i])
             {
-                delete it.ptr;
-                it.ptr = nullptr;
+                delete pArrAnims[i];
+                pArrAnims[i] = nullptr;
             }
         }
     }
@@ -75,9 +97,9 @@ public:
         }
     }
     
-/* ------------------------------------ */
-/*            핵심 추가 코드            */
-/* ------------------------------------ */
+/* ------------------------ */
+/*      핵심 추가 코드      */
+/* ------------------------ */
 public:
     void SetAnimation(UINT _iAnimState, CAnimation* _pAnimation)
     {
@@ -101,8 +123,9 @@ public:
     }
     CAnimation(double _dInterval)
         : dInterval(_dInterval)
+        , ...
     {
-    
+        ...
     }
     ~CAnimation()
     {
